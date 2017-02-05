8f18ea84a9533304717663d20599681de7301dd4
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2017-02-05 01:08:43 -0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+deis-rocket-notifier
+====================
+
+A deis webhook rocketchat notifier
+
+#### Configuration
+
+Set the following environment variables:
+
+* ROCKETCHAT_USERNAME - the username that will send messaages to ROCKETCHAT_TOPIC
+* ROCKETCHAT_PASSWORD - the password of the user that will send messages to ROCKETCHAT_TOPIC
+* ROCKETCHAT_TOPIC - the topic on which notifier will notify new releases of apps (e.g. #releases)
+* ROCKETCHAT_URL - the rocketchat url
---
+++ notifier.py
@@ -0,0 +1,33 @@
+#!flask/bin/python
+from flask import Flask
+from flask import request
+from rocketchat.api import RocketChatAPI
+import os
+
+app = Flask(__name__)
+
+def notify_rocketchat(app, release, release_summary, sha, user):
+    api.send_message(':new: *Deis({env})* {app} v#{release} - _{release_summary}_'
+            .format(app=app, release=release, release_summary=release_summary, env=env), topic)
+
+@app.route('/notify_deploy', methods=['POST'])
+def notify_deploy():
+    app = request.args.get('app')
+    release = request.args.get('release')
+    release_summary = request.args.get('release_summary')
+    sha = request.args.get('sha')
+    user = request.args.get('user')
+    print('notifying deploy app={app}, release={release}, release_summary={release_summary} topic={topic} '.format(app=app, release=release, release_summary=release_summary, topic=topic))
+    notify_rocketchat(app, release, release_summary, sha, user)
+    return ''
+
+if __name__ == '__main__':
+    env = os.environ.get('ENV', 'stag')
+    username = os.environ.get('ROCKETCHAT_USERNAME', 'user')
+    password = os.environ.get('ROCKETCHAT_PASSWORD', 'password')
+    topic = os.environ.get('ROCKETCHAT_TOPIC', 'deis-releases')
+    url = os.environ.get('ROCKETCHAT_URL', 'http://localhost:8080')
+    api = RocketChatAPI(settings={'username': username, 'password': password,
+        'domain': url})
+    print 'initializing deis-rocket-notifier with rocketchat username={username}, url={url}, password={password}'.format(username=username, url=url, password=password)
+    app.run(debug=True)
